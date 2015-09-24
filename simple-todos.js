0afbdcd2ef45bc3740cf93416cca6eb694d8d7b5
atrox3d <atrox3d@gmail.com>
2015-09-24 16:19:39 +0200

    Storing tasks in a collection

--- simple-todos.js
+++ simple-todos.js
@@ -1,16 +1,23 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
 
-  Template.body.helpers({
-    tasks: [
-        { text: "this is task 1"},
-        { text: "this is task 2"},
-        { text: "this is task 3"},
-    ]
-  });
+    Template.body.helpers({
+        /*
+            tasks: [
+                { text: "this is task 1"},
+                { text: "this is task 2"},
+                { text: "this is task 3"},
+            ]
+        */
+        tasks: function() {
+            return Tasks.find({});
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
