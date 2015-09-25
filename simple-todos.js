32e8f321cdb72efa5b06c6640b3b65551844841a
atrox3d <atrox3d@gmail.com>
2015-09-25 16:55:13 +0200

    Adding tasks with a form https://www.meteor.com/tutorials/blaze/forms-and-events

--- simple-todos.js
+++ simple-todos.js
@@ -11,7 +11,38 @@ if (Meteor.isClient) {
             ]
         */
         tasks: function() {
-            return Tasks.find({});
+            return Tasks.find({}, {sort: {createdAT: -1}});
+        }
+    });
+
+    Template.body.events({
+        "submit .new-task" : function (event) {
+
+            // console.log("event", event);
+            // console.log("event.target", event.target);
+            // console.log("event.target.text", event.target.text);
+            event.preventDefault();
+
+            if(JSON) {
+                //console.log("json");
+                //console.log("JSON.stringify", JSON.stringify);
+                //console.log(JSON.stringify({name:"bob"}));
+                console.log(JSON.stringify(event.target));
+                //console.log(event.target);
+                window.myob = event; // console.log(myob);
+
+            } else {
+                console.log("no json");
+            }
+
+            var text = event.target.text.value;
+
+            Tasks.insert({
+                text: text,
+                createdAt: new Date()
+            });
+
+            event.target.text.value = "";
         }
     });
 }
